49eb655be8ff2ec252a4d3d2bf0d5b3a4bfb3cbe
Quang Vuong <quang_vuong0409@yahoo.com>
2021-01-09 17:04:26 -0800

    Changed to HTML output via Thymeleaf

--- demo/src/main/java/com/example/demo/ReadingController.java
+++ demo/src/main/java/com/example/demo/ReadingController.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.steoreotype.Controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
